e194846399a8bb4785bdab5e3f681d4a0fe676fa
Emmanuel Ramos <emmanuelframos@gmail.com>
2018-09-18 21:36:40 -0300

    React steup

--- js/mapinit.js
+++
@@ -1,7 +0,0 @@
-function initMap() {
-    var saoPaulo = {lat: -23.533773, lng: -46.625290};
-    
-    var map = new google.maps.Map(document.getElementById('map'), {center: saoPaulo, zoom: 8});
-    
-    var marker = new google.maps.Marker({position: saoPaulo, map: map});
-}
